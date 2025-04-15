d53f37ec9ec8aab98c8a31c44aabc3dcd50ab611
rus1980707 <rus1980707@gmail.com>
2025-04-15 21:46:59 +0300

    changed

--- src/main/java/spring/bookstore/exception/EntityNotFoundException.java
+++ src/main/java/spring/bookstore/exception/EntityNotFoundException.java
@@ -3,5 +3,6 @@ package spring.bookstore.exception;
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
+
     }
 }
